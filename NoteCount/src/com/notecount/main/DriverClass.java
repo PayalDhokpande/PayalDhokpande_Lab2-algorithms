c8b5f869d6b8328345ed8a7a6fea5289d11d7508
PayalDhokpande <105939692+PayalDhokpande@users.noreply.github.com>
2022-07-11 22:05:36 +0530

    Add files via upload

---
+++ NoteCount/src/com/notecount/main/DriverClass.java
@@ -0,0 +1,36 @@
+package com.notecount.main;
+
+import java.util.Scanner;
+
+import com.notecount.implementation.MergeSortImplementation;
+import com.notecount.notescount.NotesCount;
+
+
+public class DriverClass {
+
+	public static void main(String[] args) {
+		
+		MergeSortImplementation mergeSortImplementation = new MergeSortImplementation();
+	    NotesCount notesCount = new NotesCount();
+	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the size of currency denomination ");
+		int size = sc. nextInt();
+		
+		System.out.println("Enter the currency denomination");
+		int[] notes =new int [size];
+		for(int i =0; i<size; i++) {
+		notes[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter the amount you want to pay");
+		int amount = sc. nextInt();
+		mergeSortImplementation.sort(notes,0, notes.length-1);
+		
+		notesCount.notesCountImplementation(notes, amount);
+		sc.close();
+		
+	}
+
+}
